1b0dc9315a0649e120be29a6ce14e5cf2bdf736a
pengzy <pengzy@skeqi.com>
2022-05-18 17:33:34 +0800

    2022-5-18;添加vercel部署twikoo评论

---
+++ public/js/welcome.js
@@ -0,0 +1,23 @@
+function welcome() {
+    let welcome_text='欢迎光顾本知恩的小窝!';
+    console.log(111,document.referrer)
+    console.log(222,document.domain)
+    if(document.referrer!==''){
+        let referrer=document.referrer.split("/")[2];
+        welcome_text="欢迎你，来自"+referrer.toUpperCase()+"的用户！";
+        console.log(222,document.referrer.split("/"))
+        console.log(222,document.domain)
+        if(referrer.toUpperCase()==document.domain.toUpperCase())return;
+    }
+    swal({
+        title:"欢迎！",
+        text: welcome_text+'\n打开下方音乐以获取更佳体验!',
+        imageUrl: "/img/640.png",
+        timer: 2500,
+        showConfirmButton: false
+    });
+}
+
+$(document).ready(()=>{
+    welcome();
+})
